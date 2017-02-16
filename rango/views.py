b371218ef7bdc700e532726e0dc5e2320be184e9
Wei Gao <2272657g@student.gla.ac.uk>
2017-02-16 10:39:08 +0000

    chapter 8

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
 from rango.forms import PageForm
-from django.http import HttpResponse
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
